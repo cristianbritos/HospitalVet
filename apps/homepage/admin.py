66539373e3a8d6ae0725fe4c608b85c7f70ad5de
Cristian Britos <cbritos@unlar.edu.ar>
2024-11-06 18:12:31 -0300

    vs 1.31 - Cristian

--- apps/homepage/admin.py
+++ apps/homepage/admin.py
@@ -4,6 +4,8 @@ from .models import Cliente
 from .models import Mascota
 from .models import Sala
 from .models import Turno
+from .models import *
+
 
 # Register your models here.
 admin.site.register(Veterinario)
@@ -11,3 +13,5 @@ admin.site.register(Cliente)
 admin.site.register(Mascota)
 admin.site.register(Sala)
 admin.site.register(Turno)
+admin.site.register(HistMedEnc)
+admin.site.register(HistMedDet)
